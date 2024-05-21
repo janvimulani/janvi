1876c421dbb83cee2ce6ee813d896169859bb0ee
RominMadaliya <rominskillqode@gmail.com>
2024-05-21 09:39:32 +0530

    change

---
+++ square.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int area,square;
+    cout<<"enter the value:";
+    cin>>square;
+    area=square*square;
+    cout<<"the area is:"<<area<<endl;
+}
