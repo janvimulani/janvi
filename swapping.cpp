430577efa191f4fe777f57340ae34be4bdb21e9d
RominMadaliya <rominskillqode@gmail.com>
2024-05-22 09:55:04 +0530

    change

---
+++ swapping.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    cout<<"enter the value of a:";
+    cin>>a;
+    cout<<"enter the value of b:";
+    cin>>b;
+    c=a;
+    a=b;
+    b=c;
+    cout<<"the value of a is:"<<a;
+    cout<<"the value of b is:"<<b;
+    
+}
