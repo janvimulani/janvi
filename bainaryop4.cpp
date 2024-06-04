2b6ebf9f8991f2a1c74926c055f36aa85eeb98bd
Janvi <sq.janvi@gmail.com>
2024-06-04 09:50:36 +0530

    04/06/2024

---
+++ bainaryop4.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+class box
+{
+    int l, w, h;
+
+public:
+    void setdata(int a, int b, int c)
+    {
+        l = a, w = b, h = c;
+    }
+    int getvolume()
+    {
+        return l * w * h;
+    }
+    box operator*(box &n)
+    {
+        box t;
+        t.l = l * n.l;
+        t.w = w * n.w;
+        t.h = h * n.h;
+        return t;
+    }
+};
+int main()
+{
+    box a, b, c;
+    a.setdata(7, 7, 7);
+    b.setdata(9, 9, 9);
+    cout << "volume of a is:" << a.getvolume() << endl;
+    cout << "volume of b is:" << b.getvolume() << endl;
+    c = a * b;
+    cout << "volume of c is:" << c.getvolume() << endl;
+}
---
+++ unaryop.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+class box
+{
+    int l, w, h;
+
+public:
+    void setdata(int a, int b, int c)
+    {
+        l = a, w = b, h = c;
+    }
+    int getvolume()
+    {
+        return l * w * h;
+    }
+    box operator-(box &n)
+    {
+        box t;
+        t.l = l - n.l;
+        t.w = w - n.w;
+        t.h = h - n.h;
+        return t;
+    }
+    box operator--()
+    {
+        box t;
+        t.l = -- l;
+        t.w = --w;
+        t.h = --h;
+        return t; 
+    }
+};
+int main()
+{
+    box a, b, c, d;
+    a.setdata(4, 4, 4);
+    b.setdata(2, 2, 2);
+    cout << "volume of a is:" << a.getvolume() << endl;
+    cout << "volume of b is:" << b.getvolume() << endl;
+    c = a - b;
+    cout << "volume of c is:" << c.getvolume() << endl;
+    d = --c;
+    cout << "volume of d is:" << d.getvolume() << endl;
+}
