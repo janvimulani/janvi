4ef0bb9ab057cd76db75fa6cfa75bff9af9da425
Janvi <sq.janvi@gmail.com>
2024-06-14 08:56:42 +0530

    14/6/2024

--- hirqachicalin.cpp
+++ hirqachicalin.cpp
@@ -1,29 +1,57 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 class base
 {
-    protected:
+protected:
     int a;
-    public:
+
+public:
     void setA()
     {
-        cout<<"enter a:";
-        cin>>a;
+        cout << "enter a : ";
+        cin >> a;
     }
-};  
-    class ABC: public base
+};
+class ABC : public base
+{
+    int b;
+
+public:
+    void setB()
     {
+        cout << "enter b : ";
+        cin >> b;
+    }
+    void add()
+    {
+        cout << "addition of a and b is : " << a + b << endl;
+    }
+};
 
-        int b;
-        public:
-        void set B()
-        {
-           cout<<"enter b:";
-           cin>>b; 
-        }
-        void add()
-        {
-            cout<<"addition of a and b is :"<<a+b<<endl;
-        }
+class XYZ : public base
+{
+    int c;
 
-    };
+public:
+    void setC()
+    {
+        cout << "enter c : ";
+        cin >> c;
+    }
+    void product()
+    {
+        cout << "product of a and c is : " << a * c << endl;
+    }
+};
+int main()
+{
+    ABC a;
+    a.setA();
+    a.setB();
+    a.add();
+
+    XYZ x;
+    x.setA();
+    x.setC();
+    x.product();
+}
